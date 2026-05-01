4297b411e584ad08944bde4e1d72c7b4c43fd4b4
nicollekhan <nicollekhan@yahoo.com>
2026-05-01 11:42:06 -0400

    add cookie parser

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cookieParser from 'cookie-parser';
 import { getPool } from './db/connect.js';
 import { buildAdminRouter } from './admin/index.js';
 import { startDibbsCron } from './routes/dibss.js';
@@ -35,6 +36,7 @@ app.use(cors({
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // API
